637719fbbc4f45fd8f895fe01c87de6ecb10fe9e
zzBaTmaNzz <66301505+zzBaTmaNzz@users.noreply.github.com>
2020-10-22 17:42:09 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+#define max 100
 int main()
+
 {
-    char name,country,skill;
+    char name[max],country[max],skill;
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    //scanf("%s",&name);
+	gets(name);    
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%s",&country);
+    
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    scanf("%d",&age);
+    
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    
+    printf("Thank you %s Happy Hacking",name);//Thank the user
 
     return 0;
 }
